f15cf2b2da03158922a1b135a89ac544113ededd
FahimeRohani <f.roohani@outlook.com>
2016-12-17 22:25:49 +0330

    create initialize edittext and button

--- Practice9/app/src/main/java/com/android/hosein/practice9/MainActivity.java
+++ Practice9/app/src/main/java/com/android/hosein/practice9/MainActivity.java
@@ -2,15 +2,33 @@ package com.android.hosein.practice9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextuser;
+    EditText editTextpass;
+    Button buttonlogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        config();
 
 
     }
+
+    public void config() {
+
+        editTextuser = (EditText) findViewById(R.id.user);
+        editTextpass = (EditText) findViewById(R.id.pass);
+        buttonlogin = (Button) findViewById(R.id.login);
+
+
+    }
+
+
 }
